067b58af4b4a06db64cf42687b07295ebd37fe63
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2022-06-25 22:19:24 +0900

    Time: 1 ms (98.44%), Space: 50.2 MB (38.92%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -6,8 +6,8 @@ class Solution {
     int [] ans = new int[n*2];
         
     for (int i=0; i< nums.length; i++) {
-        ans[i] = nums[i];
-        ans[i+n] = nums[i];
+        ans[i] = ans[i+n] = nums[i];
+
     }
             
      return ans;        
