ccf2b74dac844b6dff7d7e671265a5bb514aa485
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2022-06-25 20:42:03 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (81.88%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        
+        int sumValue = 0;
+        
+        sumValue = num1 + num2;
+        
+        return sumValue;
+               
+    }
+}
