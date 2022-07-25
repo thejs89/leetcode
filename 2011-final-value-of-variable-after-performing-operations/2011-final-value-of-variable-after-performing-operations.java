7056fdf6122d9818369070ce74c2a4e351bba096
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2022-07-25 19:33:50 +0900

    Time: 2 ms (46.64%), Space: 43 MB (61.10%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -2,10 +2,9 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int count = 0;
         for(int i=0; i< operations.length; i++){
-            String op = operations[i];
-            if(op.indexOf('+') > -1){
+            if(operations[i].indexOf('+') > -1){
                 count= count+1;
-            }else if(op.indexOf('-') > -1){
+            }else if(operations[i].indexOf('-') > -1){
                 count = count-1;
             }
         }
