47680617e305bb65904f22616b2acee9a7dddc79
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2022-07-27 18:54:40 +0900

    Time: 8 ms (21.23%), Space: 45.9 MB (9.88%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -2,7 +2,9 @@ class Solution {
     public int mostWordsFound(String[] sentences) {    
         int maxLen =0;
         for(int i=0; i<sentences.length; i++){
-            maxLen = Math.max(maxLen,sentences[i].split(" ").length);
+            if(maxLen < sentences[i].split(" ").length){
+                maxLen = sentences[i].split(" ").length;
+            }
         }
         return maxLen;
     }
