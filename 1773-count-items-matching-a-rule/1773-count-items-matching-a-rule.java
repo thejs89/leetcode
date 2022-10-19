34443ff9e84f3e026bbac13f59b49ea8af10ec0c
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2022-10-19 19:22:22 +0900

    Time: 3 ms (99.55%), Space: 46.9 MB (97.43%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -3,10 +3,15 @@ class Solution {
         
         int result = 0;
         
+        int location;
+        
+        if("type".equals(ruleKey)) location = 0;
+        else if("color".equals(ruleKey)) location = 1;
+        else if("name".equals(ruleKey)) location = 2;
+        else return 0;
+        
         for(int i = 0; i < items.size(); i++) {
-            if("type".equals(ruleKey) && ruleValue.equals(items.get(i).get(0))) result++; 
-            else if("color".equals(ruleKey) && ruleValue.equals(items.get(i).get(1))) result++;
-            else if("name".equals(ruleKey) && ruleValue.equals(items.get(i).get(2))) result++;
+            if(ruleValue.equals(items.get(i).get(location))) result++; 
         }
         
         return result;
