d79b1ee9cb24375519149f0a87352f22bd5a1c8d
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2024-06-13 10:02:57 +0900

    Time: 0 ms (100%), Space: 41.2 MB (92.97%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -1,8 +1,20 @@
 class Solution {
     public String truncateSentence(String s, int k) {
+        /*
         String [] arrS = s.split(" ");
         String [] arrScut = Arrays.copyOfRange(arrS, 0, k); 
         String result = String.join(" ",arrScut); 
         return result;
+        */
+        int wordCount = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                wordCount++;
+                if(wordCount == k) {
+                    return s.substring(0,i);
+                }
+            }
+        }
+        return s;
     }
 }
