cd88192bcc971b318ff36cb80b0f8412693fdbdc
Jeon JinSeong <53209392+thejs89@users.noreply.github.com>
2024-06-13 17:58:55 +0900

    Time: 128 ms (43.26%), Space: 73.6 MB (23.62%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+
+    return this.reduce((acc,cur)=> {
+        const key = fn(cur);
+
+        if(!acc[key]) acc[key] = [];
+
+        acc[key].push(cur);
+
+        return acc;
+    },{})
+
+
+
+    
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
